2dfcd6185f3a08b3a0f4b065d349a96d580e0013
Saifuddin <shaikh.saifuddin@internationalsos.com>
2024-10-16 17:51:16 +0530

    Shadcn ui added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './components/ui/button';
+import { CirclePlus } from 'lucide-react';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -19,26 +21,12 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <p> count is {count}</p>
+    <Button variant="outline" size="icon" onClick={() => setCount((count) => count + 1)}>
+      <CirclePlus  className="h-6 w-6" /> 
+    </Button>
+ 
+    
     </>
   )
 }
