5d89863c1826da1834a48fb632bd2d72644e6e3a
Saifuddin <shaikh.saifuddin@internationalsos.com>
2024-10-16 19:42:12 +0530

    card added

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,13 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from './components/ui/button';
-import { CirclePlus } from 'lucide-react';
+import "./App.css";
+import Home from "./Pages/home";
+ 
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    const initalCount = localStorage.getItem('count');
-    if (initalCount) {
-      setCount(+initalCount);
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('count', JSON.stringify(count));
-  }, [count]);
-
   return (
     <>
-    <p> count is {count}</p>
-    <Button variant="outline" size="icon" onClick={() => setCount((count) => count + 1)}>
-      <CirclePlus  className="h-6 w-6" /> 
-    </Button>
- 
-    
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
